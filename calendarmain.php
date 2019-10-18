bab9ab70192ad73ae11d820a44f28bf7ebd19e65
Samika Kikkeri <samikakikkeri@samikas-air.dhcp.wustl.edu>
2019-10-17 19:20:27 -0500

    7:20

---
+++ calendarmain.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+
+<html>
+
+    <head>
+
+    <title> CALENDAR </title>
+    
+    <script src="https://classes.engineering.wustl.edu/cse330/content/calendar.min.js" type="text/javascript"></script>
+
+    <link rel = "stylesheet" type="text/css" href ="calendarstylesheet.css">
+    <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/themes/start/jquery-ui.css"
+ 
+    type="text/css" rel="Stylesheet" /> <!-- We need the style sheet linked
+    above or the dialogs/other parts of jquery-ui won't display correctly!-->
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script><!-- The main library. 
+    Note: must be listed before the jquery-ui library -->
+
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.5/jquery-ui.min.js"></script><!-- jquery-UI 
+    hosted on Google's Ajax CDN-->
+    <!-- Note: you can download the javascript file from the link -->
+
+    </head>
+    <body>
+
+    <button id="previous" class="button"><span><</span></button>
+    <button id="next" class="button"><span>></span></button>
+
+    <script>
+
+        function updateCal(){
+            let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+            let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+            let 
+        }
