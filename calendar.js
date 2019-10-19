a2509a70496ae8f99e5a24bbfdb7e4da8fb45716
Samika Kikkeri <samikakikkeri@samikas-air.dhcp.wustl.edu>
2019-10-18 23:30:40 -0500

    huh?

--- calendar.js
+++ calendar.js
@@ -1,4 +1,8 @@
 
+//Log In
+
+
+
 let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
