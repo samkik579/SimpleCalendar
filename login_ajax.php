339366967c167922a85d8e1521623c594bfdb394
Samika Kikkeri <samikakikkeri@samikas-air.dhcp.wustl.edu>
2019-10-19 19:09:45 -0500

    7:09

--- login_ajax.php
+++ login_ajax.php
@@ -1,5 +1,7 @@
 <?php
 // login_ajax.php
+require 'database.php';
+
 
 header("Content-Type: application/json"); // Since we are sending a JSON response here (not an HTML document), set the MIME Type to application/json
 
@@ -9,13 +11,29 @@ $json_str = file_get_contents('php://input');
 $json_obj = json_decode($json_str, true);
 
 //Variables can be accessed as such:
-$username = $json_obj['username'];
-$password = $json_obj['password'];
+
+
 //This is equivalent to what you previously did with $_POST['username'] and $_POST['password']
 
-// Check to see if the username and password are valid.  (You learned how to do this in Module 3.)
+$mysqli->query(/* perform transfer */);
+// Use a prepared statement
+$stmt = $mysqli->prepare("SELECT COUNT(*), id, pass_word FROM users WHERE username=?");
+// Bind the parameter
+$stmt->bind_param('s', $username);
+$username = $json_obj['username'];
+//$user = $_POST['username'];
+$stmt->execute();
+// Bind the results
+$stmt->bind_result($cnt, $user_id, $pwd_hash);
+$stmt->fetch();
+   
+$password = $json_obj['password'];
+//$pwd_guess = $_POST['pass_word'];
+// Compare the submitted password to the actual password hash
+//echo "$pwd_guess $pwd_hash";
+//echo password_verify($pwd_guess, $pwd_hash);
 
-if( password_verify($username, $password) ){
+if($cnt == 1 && password_verify($username, $password) ){
 	session_start();
 	$_SESSION['username'] = $username;
 	$_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(32)); 
