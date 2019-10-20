b9138e455d1b3989b8d41acfe7d3a37a874bb941
Raeka Attari <raekaattari@wustl.edu>
2019-10-19 22:29:11 -0500

    10:29

--- calendar.js
+++ calendar.js
@@ -21,10 +21,10 @@ function loginAjax(event) {
     const data = { 'username': username, 'password': password };
 
     fetch("login_ajax.php", {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: { 'content-type': 'application/json' }
-        })
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'content-type': 'application/json' }
+    })
         .then(response => response.json())
         .then(data => console.log(data.success ? "You've been logged in!" : `You were not logged in ${data.message}`));
 }
@@ -39,13 +39,14 @@ function registerAjax(event) {
     const data = { 'username': username, 'password': password };
 
     fetch("registerUser.php", {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: { 'content-type': 'application/json' }
-        })
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'content-type': 'application/json' }
+    })
         .then(response => response.json())
         .then(data => console.log(data.success ? "You've been Registered!" : `You were not a user ${data.message}`));
 }
+
 
 //document.getElementById("register_btn").addEventListener("click", registerAjax, false); // Bind the AJAX call to button click
 
@@ -75,27 +76,6 @@ function updateCalendar() {
         }
     }
 
-    // console.log(currentMonth.month + " " + offset);
-    //offset is the first day of the month as a number
-    // for loop below should print out day of week 
-
-    // window.onload = function () {
-    //     this.Object.eListener();
-    // }
-
-    // let object = {
-    //     info: document.getElementsByClassName('row1'),
-
-    //     eListener: function () {
-    //         let className = this.info;
-
-    //         for (let i = 0; i < className.length; i++) {
-    //             className[i].addEventListener('click', function () { console.log(this.id + offset) });
-
-    //         }
-    //     }
-    // }
-
     let numcurrentmonth = numDays[currentMonth.month];
 
     //console.log(numcurrentmonth);
@@ -123,161 +103,136 @@ function updateCalendar() {
     document.getElementById("month").innerHTML = months[currentMonth.month];
     document.getElementById("year").innerHTML = currentMonth.year;
 
-    // $('#' + i).innerhtml(id - offset); Ids
-    // $('.') Classes
 
-    // for (var w in weeks) {
-    //     let dayNum = offset;
-    //     var days = weeks[w].getDates(); // get array of dates
-    //     console.log(days);
 
-    //     //console.log("Week starts on " + days[0]); // tells us what day weeks starts on
+    let nm = document.getElementById("next_month_btn");
+    if (nm) {
+        nm.addEventListener("click", function () {
+            currentMonth = currentMonth.nextMonth(); // Previous month would be currentMonth.prevMonth()
+            updateCalendar(); // Whenever the month is updated, we'll need to re-render the calendar in HTML
+            //alert("The new month is " + months[currentMonth.month] + " " + currentMonth.year);
+        }, false);
+    }
 
-    //     for (var d in days) {
-    //         console.log(d);
-
-    //     }
-    // }
-}
-
-// days contains normal JavaScript Date objects.
-
-//alert("Week starting on " + days[0]);
-
-// Week starts with Wed --> day 4  
-// Wed = 1 (id 4 =1); 
-// let offset = (id -1)
-// 28 days in month 
-// for(id from 4 - + 28) 
-//     box = id - offset  
-
-let nm = document.getElementById("next_month_btn");
-if (nm) {
-    nm.addEventListener("click", function () {
-        currentMonth = currentMonth.nextMonth(); // Previous month would be currentMonth.prevMonth()
-        updateCalendar(); // Whenever the month is updated, we'll need to re-render the calendar in HTML
-        //alert("The new month is " + months[currentMonth.month] + " " + currentMonth.year);
-    }, false);
-}
-
-let pm = document.getElementById("prev_month_btn");
-if (pm) {
-    pm.addEventListener("click", function () {
-        currentMonth = currentMonth.prevMonth(); // Previous month would be currentMonth.prevMonth()
-        updateCalendar(); // Whenever the month is updated, we'll need to re-render the calendar in HTML
-        // alert("The new month is " + months[currentMonth.month] + " " + currentMonth.year);
-    }, false);
-}
+    let pm = document.getElementById("prev_month_btn");
+    if (pm) {
+        pm.addEventListener("click", function () {
+            currentMonth = currentMonth.prevMonth(); // Previous month would be currentMonth.prevMonth()
+            updateCalendar(); // Whenever the month is updated, we'll need to re-render the calendar in HTML
+            // alert("The new month is " + months[currentMonth.month] + " " + currentMonth.year);
+        }, false);
+    }
 
 
 
-(function () {
-    "use strict";
 
-    /* Date.prototype.deltaDays(n)
+    (function () {
+        "use strict";
+
+        /* Date.prototype.deltaDays(n)
+         * 
+         * Returns a Date object n days in the future.
+         */
+        Date.prototype.deltaDays = function (n) {
+            // relies on the Date object to automatically wrap between months for us
+            return new Date(this.getFullYear(), this.getMonth(), this.getDate() + n);
+        };
+
+        /* Date.prototype.getSunday()
+         * 
+         * Returns the Sunday nearest in the past to this date (inclusive)
+         */
+        Date.prototype.getSunday = function () {
+            return this.deltaDays(-1 * this.getDay());
+        };
+    }());
+
+    /** Week
      * 
-     * Returns a Date object n days in the future.
-     */
-    Date.prototype.deltaDays = function (n) {
-        // relies on the Date object to automatically wrap between months for us
-        return new Date(this.getFullYear(), this.getMonth(), this.getDate() + n);
-    };
-
-    /* Date.prototype.getSunday()
+     * Represents a week.
      * 
-     * Returns the Sunday nearest in the past to this date (inclusive)
+     * Functions (Methods):
+     *	.nextWeek() returns a Week object sequentially in the future
+     *	.prevWeek() returns a Week object sequentially in the past
+     *	.contains(date) returns true if this week's sunday is the same
+     *		as date's sunday; false otherwise
+     *	.getDates() returns an Array containing 7 Date objects, each representing
+     *		one of the seven days in this month
      */
-    Date.prototype.getSunday = function () {
-        return this.deltaDays(-1 * this.getDay());
-    };
-}());
+    function Week(initial_d) {
+        "use strict";
 
-/** Week
- * 
- * Represents a week.
- * 
- * Functions (Methods):
- *	.nextWeek() returns a Week object sequentially in the future
- *	.prevWeek() returns a Week object sequentially in the past
- *	.contains(date) returns true if this week's sunday is the same
- *		as date's sunday; false otherwise
- *	.getDates() returns an Array containing 7 Date objects, each representing
- *		one of the seven days in this month
- */
-function Week(initial_d) {
-    "use strict";
-
-    this.sunday = initial_d.getSunday();
+        this.sunday = initial_d.getSunday();
 
 
-    this.nextWeek = function () {
-        return new Week(this.sunday.deltaDays(7));
-    };
+        this.nextWeek = function () {
+            return new Week(this.sunday.deltaDays(7));
+        };
 
-    this.prevWeek = function () {
-        return new Week(this.sunday.deltaDays(-7));
-    };
+        this.prevWeek = function () {
+            return new Week(this.sunday.deltaDays(-7));
+        };
 
-    this.contains = function (d) {
-        return (this.sunday.valueOf() === d.getSunday().valueOf());
-    };
+        this.contains = function (d) {
+            return (this.sunday.valueOf() === d.getSunday().valueOf());
+        };
 
-    this.getDates = function () {
-        var dates = [];
-        for (var i = 0; i < 7; i++) {
-            dates.push(this.sunday.deltaDays(i));
-        }
-        return dates;
-    };
-}
+        this.getDates = function () {
+            var dates = [];
+            for (var i = 0; i < 7; i++) {
+                dates.push(this.sunday.deltaDays(i));
+            }
+            return dates;
+        };
+    }
 
-/** Month
- * 
- * Represents a month.
- * 
- * Properties:
- *	.year == the year associated with the month
- *	.month == the month number (January = 0)
- * 
- * Functions (Methods):
- *	.nextMonth() returns a Month object sequentially in the future
- *	.prevMonth() returns a Month object sequentially in the past
- *	.getDateObject(d) returns a Date object representing the date
- *		d in the month
- *	.getWeeks() returns an Array containing all weeks spanned by the
- *		month; the weeks are represented as Week objects
- */
-function Month(year, month) {
-    "use strict";
+    /** Month
+     * 
+     * Represents a month.
+     * 
+     * Properties:
+     *	.year == the year associated with the month
+     *	.month == the month number (January = 0)
+     * 
+     * Functions (Methods):
+     *	.nextMonth() returns a Month object sequentially in the future
+     *	.prevMonth() returns a Month object sequentially in the past
+     *	.getDateObject(d) returns a Date object representing the date
+     *		d in the month
+     *	.getWeeks() returns an Array containing all weeks spanned by the
+     *		month; the weeks are represented as Week objects
+     */
+    function Month(year, month) {
+        "use strict";
 
-    this.year = year;
-    this.month = month;
+        this.year = year;
+        this.month = month;
 
-    this.nextMonth = function () {
-        return new Month(year + Math.floor((month + 1) / 12), (month + 1) % 12);
-    };
+        this.nextMonth = function () {
+            return new Month(year + Math.floor((month + 1) / 12), (month + 1) % 12);
+        };
 
-    this.prevMonth = function () {
-        return new Month(year + Math.floor((month - 1) / 12), (month + 11) % 12);
-    };
+        this.prevMonth = function () {
+            return new Month(year + Math.floor((month - 1) / 12), (month + 11) % 12);
+        };
 
-    this.getDateObject = function (d) {
-        return new Date(this.year, this.month, d);
-    };
+        this.getDateObject = function (d) {
+            return new Date(this.year, this.month, d);
+        };
 
-    this.getWeeks = function () {
-        var firstDay = this.getDateObject(1);
-        var lastDay = this.nextMonth().getDateObject(0);
+        this.getWeeks = function () {
+            var firstDay = this.getDateObject(1);
+            var lastDay = this.nextMonth().getDateObject(0);
 
-        var weeks = [];
-        var currweek = new Week(firstDay);
-        weeks.push(currweek);
-        while (!currweek.contains(lastDay)) {
-            currweek = currweek.nextWeek();
+            var weeks = [];
+            var currweek = new Week(firstDay);
             weeks.push(currweek);
-        }
+            while (!currweek.contains(lastDay)) {
+                currweek = currweek.nextWeek();
+                weeks.push(currweek);
+            }
 
-        return weeks;
-    };
-}
+            return weeks;
+        };
+    }
 
